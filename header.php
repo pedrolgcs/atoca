5d06eabc6c0ca24a522688a7095ad95160f7807c
Pedro Henrique <pedro.lg.cs@gmail.com>
2017-04-20 21:15:44 -0300

    criando arquivos php

---
+++ header.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="icon" href="images/imagemin/logo.png" />
+    <link rel="stylesheet" href="css/plugins.css">
+    <link rel="stylesheet" href="css/geral.css">
+    <link rel="stylesheet" href="css/home.css">
+    <title>AToca</title>
+</head>
+
+<body>
+    <!-- menu superior !-->
+    <nav class="navbar navbar-default navbar-fixed-top">
+        <div class="container">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1"
+                    aria-expanded="false">
+                  <span class="sr-only">Toggle navigation</span>
+                  <span class="icon-bar"></span>
+                  <span class="icon-bar"></span>
+                  <span class="icon-bar"></span>
+               </button>
+                <!-- <a class="navbar-brand logo" href="#">AToca</a> !-->
+                <a href="index.html">
+                    <img src="images/imagemin/logo.png" class="img-responsive wow tada" data-wow-duration="2s" data-wow-delay="0.3s">
+                </a>
+            </div>
+            <div class="collapse navbar-collapse wow lightSpeedIn" data-wow-duration="1.5s" data-wow-delay="0.3s" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav navbar-right">
+                    <li><a href="index.html">início <span class="sr-only">(current)</span></a></li>
+                    <li><a href="sobre.html">sobre</a></li>
+                    <li><a href="#">posts</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <!-- nome, descricao e redes sociais !-->
+    <header>
+        <div class="container">
+            <div class="descricao">
+                <h1>AToca</h1>
+                <p>
+                    'Nós te mostraremos até onde vai a toca do coelho'
+                </p>
+                <p>
+                    <i class="fa fa-cogs" aria-hidden="true"></i> -
+                    <i class="fa fa-battery-three-quarters" aria-hidden="true"></i> -
+                    <i class="fa fa-rocket" aria-hidden="true"></i>
+                </p>
+            </div>
+        </div>
+    </header>
+    <!-- texto principal !-->
+    <main>
